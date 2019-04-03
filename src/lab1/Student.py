0fd2aa0267246220d866ba9f9f005fe37b51f073
Daniel-Heyde <dpheyde@gmail.com>
2019-04-03 11:39:11 -0700

    Added student dict object

--- src/lab1/Student.py
+++ src/lab1/Student.py
@@ -1,10 +1,10 @@
-
 class Student:
     """class for student object"""
 
     def __init__(self, line):
         # parse given file line into student object
         studentInfo = line.split(",")
+        self.line = line
         self.stLastName = studentInfo[0]
         self.stFirstName = studentInfo[1]
         self.grade = studentInfo[2]
@@ -13,3 +13,6 @@ class Student:
         self.gpa = studentInfo[5]
         self.tLastName = studentInfo[6]
         self.tFirstName = studentInfo[7]
+
+    def __repr__(self):
+        return "("+self.line+")"
---
+++ src/lab1/StudentDicts.py
@@ -0,0 +1,43 @@
+import Student
+
+class StudentDicts:
+    by_st_last_name = {}
+    by_t_last_name = {}
+    by_bus = {}
+    by_grade = {}
+    by_gpa = {}
+
+    def __init__(self, filename):
+        students = self.read_data_from_file(filename)
+        self.create_dictionaries(students)
+
+    # create dictionaries from list of student objects
+    def create_dictionaries(self, studentList):
+        for student in studentList:
+            self.add_to_dict(student, self.by_st_last_name, student.stLastName)
+            self.add_to_dict(student, self.by_t_last_name, student.tLastName)
+            self.add_to_dict(student, self.by_bus, student.bus)
+            self.add_to_dict(student, self.by_grade, student.grade)
+            self.add_to_dict(student, self.by_gpa, student.gpa)
+        print(self.by_st_last_name)
+        print(self.by_t_last_name)
+        print(self.by_bus)
+        print(self.by_grade)
+        print(self.by_gpa)
+
+    def add_to_dict(self, student, dict, key):
+        if key in dict:
+            dict[key].append(student)
+        else:
+            dict[key] = [student]
+
+    # read data file line by line
+    def read_data_from_file(self, filename):
+        file = open(filename, "r")
+        studentList = []
+        for line in file:
+            studentList.append(Student.Student(line.strip()))
+        return studentList
+
+if __name__ == "__main__":
+    StudentDicts("students.txt")
