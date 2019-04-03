0fc30a4df7e2e612859858af3f1e186595447f85
Flo Costa <flocosta1@gmail.com>
2019-04-03 12:00:29 -0700

    added Commands class

---
+++ src/lab1/Commands.py
@@ -0,0 +1,21 @@
+
+class Commands:
+    """class implementing search commands"""
+
+    def searchStudent(self, lastname, bus, data):
+        # method for searching given a student lastname and an optional boolean bus option
+
+    def searchTeacher(self, lastname, data):
+        # method for searching given a teacher lastname
+
+    def searchBus(self, bus):
+        # method for searching given a bus route number
+
+    def searchGrade(self, grade, high, low):
+        # method for searching given a grade with boolean high and low options
+
+    def searchAverage(self, grade):
+        # method for searchng given a grade
+
+    def info(self):
+        # method for info command
