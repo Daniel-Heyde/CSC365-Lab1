1feaded1e9cd0b416ebd693ec5d4163b74692e1f
Flo Costa <flocosta1@gmail.com>
2019-04-04 16:55:39 -0700

    formatting

--- src/lab1/Commands.py
+++ src/lab1/Commands.py
@@ -4,18 +4,92 @@ class Commands:
 
     def searchStudent(self, lastname, bus, data):
         # method for searching given a student lastname and an optional boolean bus option
+        for key in data.by_st_last_name:
+            if key == lastname:
+                student = data.by_st_last_name[lastname]
+                print("Student Last Name: " + student.stLastName)
+                print("Student First Name: " + student.stFirstName)
+                if bus is True:
+                    print("Bus Route:" + student.bus)
+                else:
+                    print("Grade: " + student.grade)
+                    print("Classroom Assignment: " + student.classroom)
+                    print("Teacher Last Name: " + student.tLastName)
+                    print("Teacher First Name: " + student.tFirstName)
+                print("\n")
+        print("\n")
 
     def searchTeacher(self, lastname, data):
         # method for searching given a teacher lastname
+        for key in data.by_t_last_name:
+            if key == lastname:
+                student = data.by_t_last_name[lastname]
+                print("Student Last Name: " + student.stLastName)
+                print("Student First Name: " + student.stFirstName)
+                print("\n")
+        print("\n")
 
-    def searchBus(self, bus):
+    def searchBus(self, bus, data):
         # method for searching given a bus route number
+        for key in data.by_bus:
+            if key == bus:
+                student = data.by_bus[bus]
+                print("Student Last Name: " + student.stLastName)
+                print("Student First Name: " + student.stFirstName)
+                print("Grade:")
+                print("Grade: " + student.grade)
+                print("Classroom Assignment: " + student.classroom)
+                print("\n")
+        print("\n")
 
-    def searchGrade(self, grade, high, low):
+    def searchGrade(self, grade, high, low, data):
         # method for searching given a grade with boolean high and low options
+        highestGPA = None
+        lowestGPA = None
 
-    def searchAverage(self, grade):
+        for key in data.by_grade:
+            if key == grade:
+                student = data.by_grade[grade]
+                if high is True:
+                    if highestGPA is None or student.gpa > highestGPA:
+                        highestGPA = student
+                elif low is True:
+                    if lowestGPA is None or student.gpa < lowestGPA:
+                        lowestGPA = student
+
+        print("Student Last Name: " + student.stLastName)
+        print("Student First Name: " + student.stFirstName)
+        print("GPA: " + student.gpa)
+        print("Teacher Last Name: " + student.tLastName)
+        print("Teacher First Name: " + student.tFirstName)
+        print("Bus: " + student.bus)
+        print("\n")
+
+    def searchAverage(self, grade, data):
         # method for searchng given a grade
+        sum = 0
+        num_students = 0
 
-    def info(self):
+        for key in data.by_grade:
+            if key == grade:
+                student = data.by_grade[grade]
+                sum += student.gpa
+                num_students += 1
+
+        avgGPA = sum / num_students
+
+        print("Grade: " + grade)
+        print("Average GPA: " + avgGPA)
+        print("\n\n")
+
+    def info(self, data):
         # method for info command
+        for grade in range(0,7,1):
+            count = 0
+            for key in data.by_grade:
+                if key == grade:
+                    count += 1
+            print("Grade: " + count)
+        print("\n")
+
+
