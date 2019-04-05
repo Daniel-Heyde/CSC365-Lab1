71626cb222890d1dc468cbf916c16dd8a3afabf9
Steven Sun <hsun03@calpoly.edu>
2019-04-05 10:54:22 -0700

    change parser

--- src/lab1/Parser.py
+++ src/lab1/Parser.py
@@ -14,10 +14,10 @@ def parser(studentDict):
     
         if parsedInput[0] == "S" or parsedInput[0]=="Student":
             if len(parsedInput)==2:
-                Commands.searchStudent(parsedInput[1],False,studentDict)
+                searchStudent(parsedInput[1],False,studentDict)
             elif len(parsedInput)==3:
                 if parsedInput[1]=="B" or parsedInput[1]=="Bus":
-                    Commands.searchStudent(parsedInput[1],True,studentDict)
+                    searchStudent(parsedInput[1],True,studentDict)
                 else:
                     print("Usage: S[tudent] <lastname> B[us]")
             else:
@@ -25,18 +25,18 @@ def parser(studentDict):
     
         elif parsedInput[0] == "T" or parsedInput[0] == "Teacher":
             if len(parsedInput)==2:
-                Commands.searchTeacher(parsedInput[1],True,studentDict)
+                searchTeacher(parsedInput[1],True,studentDict)
             else:
                 print("Usage: T[eacher] <lastname>")
     
         elif parsedInput[0] == "G" or parsedInput[0] == "Grade":
             if len(parsedInput)==2:
-                Commands.searchGrade(parsedInput[1],False, False, studentDict)
+                searchGrade(parsedInput[1],False, False, studentDict)
             elif len(parsedInput)==3:
                 if parsedInput[2]== "H" or parsedInput[2]=="High":
-                    Commands.searchGrade(parsedInput[1],True, False, studentDict)
+                    searchGrade(parsedInput[1],True, False, studentDict)
                 elif parsedInput[2]=="L" or parsedInput[2]=="Low":
-                    Commands.searchGrade(parsedInput[1],False,True, studentDict)
+                    searchGrade(parsedInput[1],False,True, studentDict)
                 else:
                     print("Usage: G[rade] <Number> H[igh] or G[rade] <Number L[ow]>")
             else:
@@ -44,18 +44,18 @@ def parser(studentDict):
     
         elif parsedInput[0] == "B" or parsedInput[0] == "Bus":
             if len(parsedInput)==2:
-                Commands.searchBus(parsedInput[1],studentDict)
+                searchBus(parsedInput[1],studentDict)
             else:
                 print("Usage: B[us] <Number>")
     
         elif parsedInput[0] == "A" or parsedInput[0] == "Average":
             if len(parsedInput)==2:
-                Commands.searchAverage(parsedInput[1],studentDict)
+                searchAverage(parsedInput[1],studentDict)
             else:
                 print("Usage: A[verage] <Number>")
     
         elif parsedInput[0] == "I" or parsedInput[0] == "Info":
-            Commands.searchInfo(studentDict)
+            searchInfo(studentDict)
     
         elif parsedInput[0] == "Q" or parsedInput[0] == "Quit":
             break
